a2be5b2a607ffda8b2e9df558b9fb7617c7d9b0e
Aswanth Alakkadan <aswanth682@gmail.com>
2023-10-24 20:47:11 +0530

    tailwind setup

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div>NETFLIX</div>
+    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+      Click Me
+    </button>
     </div>
   );
 }
