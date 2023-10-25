bb9c9a3a048038ad534f12e2c0ea5c5f5f602a64
Aswanth Alakkadan <aswanth682@gmail.com>
2023-10-25 13:59:03 +0530

    router setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import { BrowserRouter, Route, Router, Routes } from 'react-router-dom';
 import LandingPageWrapper from './Components/landingpage/LandingPageWrapper';
 function App() {
   return (
     <div className="App">
-      <LandingPageWrapper />
+    <BrowserRouter>
+    <Routes>
+    <Route path='/' element={<LandingPageWrapper />}></Route>
+    </Routes>
+    </BrowserRouter>
     </div>
   );
 }
