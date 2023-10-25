c1a76ccfe0ec9b1050deae28b0c615cbbda9da35
Aswanth Alakkadan <aswanth682@gmail.com>
2023-10-25 16:12:21 +0530

    navbar done

--- src/Components/mainpage/MainPageNav.jsx
+++ src/Components/mainpage/MainPageNav.jsx
@@ -13,17 +13,41 @@ export default function MainPageNav() {
         window.addEventListener('scroll', listenScrollEvent)
     })
   return (
-    <div className='main'  style={{backgroundColor:color}}>
-    <div className="title">
-    <img className='logo' src='https://res.cloudinary.com/dihrwghx2/image/upload/v1698213282/netflix/d1auoakl5foipexqnytg.png' alt="" />
-    </div>
-    <div className="noti">
-       <a href="/home">Home</a>  <a href="/abt">TVShows</a>   <a href="/con">Movies</a>  <a href="/con">Series</a> 
-   
-    </div>
-    <div className="profile">
-    <img src='https://i.pinimg.com/originals/0d/dc/ca/0ddccae723d85a703b798a5e682c23c1.png'alt="" />
-    </div>
+    <div style={{backgroundColor:color}}>
+  <div class="container mx-auto">
+    <nav class="flex items-center p-1 ">
+      <div class="flex items-center justify-start p-4 ">
+      <img className='logo' src='https://res.cloudinary.com/dihrwghx2/image/upload/v1698213282/netflix/d1auoakl5foipexqnytg.png' alt="" />
+      </div>
+
+      <div class="sm:hidden ml-56">
+        <button id="open-menu" class="text-white">
+          <svg
+            class="w-6 h-6"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M4 6h16M4 12h16m-7 6h7"
+            ></path>
+          </svg>
+        </button>
+      </div>
+
+      <div class="hidden sm:flex justify-start">
+        <a href="/" class="noti">Home</a>
+        <a href="/" class="noti">TVShows</a>
+        <a href="/" class="noti">Movies</a>
+        <a href="/" class="noti">Series</a>
+      </div>
+    </nav>
+  </div>
 </div>
+
   )
 }
